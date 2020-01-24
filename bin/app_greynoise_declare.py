d63182db95984f140da16fffcad4352000f13de0
Andrew Morris <andrew@greynoise.io>
2020-01-24 13:07:29 -0500

    update app name

--- bin/app_greynoise_declare.py
+++ bin/app_greynoise_declare.py
@@ -7,7 +7,7 @@ import os
 import sys
 import re
 
-ta_name = 'app-greynoise'
+ta_name = 'SA-GreyNoise'
 ta_lib_name = 'app_greynoise'
 pattern = re.compile(r"[\\/]etc[\\/]apps[\\/][^\\/]+[\\/]bin[\\/]?$")
 new_paths = [path for path in sys.path if not pattern.search(path) or ta_name in path]

1ab23ea0625e4cff8a293e724ef39eaa60b06a6d
Andrew Morris <andrew@greynoise.io>
2020-02-03 16:15:10 -0500

    Pull over library source code

---
+++ bin/SA_GreyNoise/greynoise/api/__init__.py
@@ -0,0 +1,332 @@
+"""GreyNoise API client."""
+
+import re
+from collections import OrderedDict
+
+import cachetools
+import more_itertools
+import requests
+import structlog
+
+from greynoise.__version__ import __version__
+from greynoise.api.analyzer import Analyzer
+from greynoise.api.filter import Filter
+from greynoise.exceptions import RateLimitError, RequestFailure
+from greynoise.util import configure_logging, load_config, validate_ip
+
+if not structlog.is_configured():
+    configure_logging()
+LOGGER = structlog.get_logger()
+
+
+class GreyNoise(object):
+
+    """GreyNoise API client.
+
+    :param api_key: Key use to access the API.
+    :type api_key: str
+    :param timeout: API requests timeout in seconds.
+    :type timeout: int
+
+    """
+
+    NAME = "GreyNoise"
+    API_VERSION = "v2"
+    EP_GNQL = "experimental/gnql"
+    EP_GNQL_STATS = "experimental/gnql/stats"
+    EP_INTERESTING = "interesting/{ip_address}"
+    EP_NOISE_MULTI = "noise/multi/quick"
+    EP_NOISE_CONTEXT = "noise/context/{ip_address}"
+    EP_NOT_IMPLEMENTED = "request/{subcommand}"
+    UNKNOWN_CODE_MESSAGE = "Code message unknown: {}"
+    CODE_MESSAGES = {
+        "0x00": "IP has never been observed scanning the Internet",
+        "0x01": "IP has been observed by the GreyNoise sensor network",
+        "0x02": (
+            "IP has been observed scanning the GreyNoise sensor network, "
+            "but has not completed a full connection, meaning this can be spoofed"
+        ),
+        "0x03": (
+            "IP is adjacent to another host that has been directly observed "
+            "by the GreyNoise sensor network"
+        ),
+        "0x04": "RESERVED",
+        "0x05": "IP is commonly spoofed in Internet-scan activity",
+        "0x06": (
+            "IP has been observed as noise, but this host belongs to a cloud provider "
+            "where IPs can be cycled frequently"
+        ),
+        "0x07": "IP is invalid",
+        "0x08": (
+            "IP was classified as noise, but has not been observed "
+            "engaging in Internet-wide scans or attacks in over 60 days"
+        ),
+    }
+
+    CACHE_MAX_SIZE = 1000
+    CACHE_TTL = 3600
+    IP_QUICK_CHECK_CACHE = cachetools.TTLCache(maxsize=CACHE_MAX_SIZE, ttl=CACHE_TTL)
+    IP_CONTEXT_CACHE = cachetools.TTLCache(maxsize=CACHE_MAX_SIZE, ttl=CACHE_TTL)
+
+    IP_QUICK_CHECK_CHUNK_SIZE = 1000
+
+    IPV4_REGEX = re.compile(
+        r"(?:{octet}\.){{3}}{octet}".format(
+            octet=r"(?:(?:25[0-5])|(?:2[0-4]\d)|(?:1?\d?\d))"
+        )
+    )
+
+    def __init__(
+        self,
+        api_key=None,
+        api_server=None,
+        timeout=None,
+        use_cache=True,
+        integration_name=None,
+    ):
+        if any(
+            configuration_value is None
+            for configuration_value in (api_key, timeout, api_server)
+        ):
+            config = load_config()
+            if api_key is None:
+                api_key = config["api_key"]
+            if api_server is None:
+                api_server = config["api_server"]
+            if timeout is None:
+                timeout = config["timeout"]
+        self.api_key = api_key
+        self.api_server = api_server
+        self.timeout = timeout
+        self.use_cache = use_cache
+        self.integration_name = integration_name
+        self.session = requests.Session()
+
+    def _request(self, endpoint, params=None, json=None, method="get"):
+        """Handle the requesting of information from the API.
+
+        :param endpoint: Endpoint to send the request to
+        :type endpoint: str
+        :param params: Request parameters
+        :type param: dict
+        :param json: Request's JSON payload
+        :type json: dict
+        :param method: Request method name
+        :type method: str
+        :returns: Response's JSON payload
+        :rtype: dict
+        :raises RequestFailure: when HTTP status code is not 2xx
+
+        """
+        if params is None:
+            params = {}
+
+        user_agent_parts = ["GreyNoise/{}".format(__version__)]
+        if self.integration_name:
+            user_agent_parts.append("({})".format(self.integration_name))
+        headers = {
+            "User-Agent": " ".join(user_agent_parts),
+            "key": self.api_key,
+        }
+        url = "/".join([self.api_server, self.API_VERSION, endpoint])
+        LOGGER.debug(
+            "Sending API request...",
+            url=url,
+            method=method,
+            headers=headers,
+            params=params,
+            json=json,
+        )
+        request_method = getattr(self.session, method)
+        response = request_method(
+            url, headers=headers, timeout=self.timeout, params=params, json=json
+        )
+        content_type = response.headers.get("Content-Type", "")
+        if "application/json" in content_type:
+            body = response.json()
+        else:
+            body = response.text
+
+        LOGGER.debug(
+            "API response received",
+            status_code=response.status_code,
+            content_type=content_type,
+            body=body,
+        )
+
+        if response.status_code == 429:
+            raise RateLimitError()
+        if response.status_code >= 400:
+            raise RequestFailure(response.status_code, body)
+
+        return body
+
+    def analyze(self, text):
+        """Aggregate stats related to IP addresses from a given text.
+
+        :param text: Text input
+        :type text: file-like | str
+        :return: Aggregated stats for all the IP addresses found.
+        :rtype: dict
+
+        """
+        analyzer = Analyzer(self)
+        return analyzer.analyze(text)
+
+    def filter(self, text, noise_only=False):
+        """Filter lines that contain IP addresses from a given text.
+
+        :param text: Text input
+        :type text: file-like | str
+        :param noise_only:
+            If set, return only lines that contain IP addresses classified as noise,
+            otherwise, return lines that contain IP addresses not classified as noise.
+        :type noise_only: bool
+        :return: Iterator that yields lines in chunks
+        :rtype: iterable
+
+        """
+        filter = Filter(self)
+        for filtered_chunk in filter.filter(text, noise_only=noise_only):
+            yield filtered_chunk
+
+    def interesting(self, ip_address):
+        """Report an IP as "interesting".
+
+        :param ip_address: IP address to report as "interesting".
+        :type ip_address: str
+
+        """
+        LOGGER.debug(
+            "Reporting interesting IP: %s...", ip_address, ip_address=ip_address
+        )
+        validate_ip(ip_address)
+
+        endpoint = self.EP_INTERESTING.format(ip_address=ip_address)
+        response = self._request(endpoint, method="post")
+        return response
+
+    def ip(self, ip_address):
+        """Get context associated with an IP address.
+
+        :param ip_address: IP address to use in the look-up.
+        :type ip_address: str
+        :return: Context for the IP address.
+        :rtype: dict
+
+        """
+        LOGGER.debug("Getting context for %s...", ip_address, ip_address=ip_address)
+        validate_ip(ip_address)
+
+        endpoint = self.EP_NOISE_CONTEXT.format(ip_address=ip_address)
+        if self.use_cache:
+            cache = self.IP_CONTEXT_CACHE
+            response = (
+                cache[ip_address]
+                if ip_address in self.IP_CONTEXT_CACHE
+                else cache.setdefault(ip_address, self._request(endpoint))
+            )
+        else:
+            response = self._request(endpoint)
+
+        if "ip" not in response:
+            response["ip"] = ip_address
+
+        return response
+
+    def not_implemented(self, subcommand_name):
+        """Send request for a not implemented CLI subcommand.
+
+        :param subcommand_name: Name of the CLI subcommand
+        :type subcommand_name: str
+
+        """
+        endpoint = self.EP_NOT_IMPLEMENTED.format(subcommand=subcommand_name)
+        response = self._request(endpoint)
+        return response
+
+    def query(self, query, size=None, scroll=None):
+        """Run GNQL query."""
+        LOGGER.debug(
+            "Running GNQL query: %s...", query, query=query, size=size, scroll=scroll
+        )
+        params = {"query": query}
+        if size is not None:
+            params["size"] = size
+        if scroll is not None:
+            params["scroll"] = scroll
+        response = self._request(self.EP_GNQL, params=params)
+        return response
+
+    def quick(self, ip_addresses):
+        """Get activity associated with one or more IP addresses.
+
+        :param ip_addresses: One or more IP addresses to use in the look-up.
+        :type ip_addresses: str | list
+        :return: Bulk status information for IP addresses.
+        :rtype: dict
+
+        """
+        if isinstance(ip_addresses, str):
+            ip_addresses = [ip_addresses]
+
+        LOGGER.debug("Getting noise status...", ip_addresses=ip_addresses)
+        ip_addresses = [
+            ip_address
+            for ip_address in ip_addresses
+            if validate_ip(ip_address, strict=False)
+        ]
+
+        if self.use_cache:
+            cache = self.IP_QUICK_CHECK_CACHE
+            # Keep the same ordering as in the input
+            ordered_results = OrderedDict(
+                (ip_address, cache.get(ip_address)) for ip_address in ip_addresses
+            )
+            api_ip_addresses = [
+                ip_address
+                for ip_address, result in ordered_results.items()
+                if result is None
+            ]
+            if api_ip_addresses:
+                api_results = []
+                chunks = more_itertools.chunked(
+                    api_ip_addresses, self.IP_QUICK_CHECK_CHUNK_SIZE
+                )
+                for chunk in chunks:
+                    api_result = self._request(self.EP_NOISE_MULTI, json={"ips": chunk})
+                    if isinstance(api_result, list):
+                        api_results.extend(api_result)
+                    else:
+                        api_results.append(api_result)
+
+                for api_result in api_results:
+                    ip_address = api_result["ip"]
+                    ordered_results[ip_address] = cache.setdefault(
+                        ip_address, api_result
+                    )
+            results = list(ordered_results.values())
+        else:
+            results = []
+            chunks = more_itertools.chunked(
+                ip_addresses, self.IP_QUICK_CHECK_CHUNK_SIZE
+            )
+            for chunk in chunks:
+                result = self._request(self.EP_NOISE_MULTI, json={"ips": chunk})
+                if isinstance(result, list):
+                    results.extend(result)
+                else:
+                    results.append(result)
+
+        for result in results:
+            code = result["code"]
+            result["code_message"] = self.CODE_MESSAGES.get(
+                code, self.UNKNOWN_CODE_MESSAGE.format(code)
+            )
+        return results
+
+    def stats(self, query):
+        """Run GNQL stats query."""
+        LOGGER.debug("Running GNQL stats query: %s...", query, query=query)
+        response = self._request(self.EP_GNQL_STATS, params={"query": query})
+        return response
---
+++ bin/SA_GreyNoise/greynoise/cli/subcommand.py
@@ -0,0 +1,227 @@
+"""CLI subcommands."""
+
+import platform
+import sys
+
+import click
+
+from greynoise.__version__ import __version__
+from greynoise.cli.decorator import (
+    echo_result,
+    gnql_command,
+    handle_exceptions,
+    ip_lookup_command,
+    not_implemented_command,
+    pass_api_client,
+)
+from greynoise.cli.formatter import ANSI_MARKUP
+from greynoise.cli.helper import get_ip_addresses, get_queries
+from greynoise.cli.parameter import ip_addresses_parameter
+from greynoise.util import CONFIG_FILE, DEFAULT_CONFIG, save_config
+
+
+@not_implemented_command
+def account():
+    """View information about your GreyNoise account."""
+
+
+@not_implemented_command
+def alerts():
+    """List, create, delete, and manage your GreyNoise alerts."""
+
+
+@click.command()
+@click.option("-k", "--api-key", help="Key to include in API requests")
+@click.option("-i", "--input", "input_file", type=click.File(), help="Input file")
+@click.option(
+    "-o", "--output", "output_file", type=click.File(mode="w"), help="Output file"
+)
+@click.option(
+    "-f",
+    "--format",
+    "output_format",
+    type=click.Choice(["json", "txt", "xml"]),
+    default="txt",
+    help="Output format",
+)
+@click.option("-v", "--verbose", count=True, help="Verbose output")
+@pass_api_client
+@echo_result
+@click.pass_context
+@handle_exceptions
+def analyze(
+    context, api_client, api_key, input_file, output_file, output_format, verbose
+):
+    """Analyze the IP addresses in a log file, stdin, etc."""
+    if input_file is None:
+        if sys.stdin.isatty():
+            output = [
+                context.command.get_usage(context),
+                (
+                    "Error: at least one text file must be passed "
+                    "either through the -i/--input_file option or through a shell pipe."
+                ),
+            ]
+            click.echo("\n\n".join(output))
+            context.exit(-1)
+        else:
+            input_file = click.open_file("-")
+    if output_file is None:
+        output_file = click.open_file("-", mode="w")
+
+    result = api_client.analyze(input_file)
+    return result
+
+
+@not_implemented_command
+def feedback():
+    """Send feedback directly to the GreyNoise team."""
+
+
+@click.command()
+@click.option("-k", "--api-key", help="Key to include in API requests")
+@click.option("-i", "--input", "input_file", type=click.File(), help="Input file")
+@click.option(
+    "-o", "--output", "output_file", type=click.File(mode="w"), help="Output file"
+)
+@click.option(
+    "--noise-only", is_flag=True, help="Select lines containing noisy addresses"
+)
+@pass_api_client
+@click.pass_context
+@handle_exceptions
+def filter(context, api_client, api_key, input_file, output_file, noise_only):
+    """Filter the noise from a log file, stdin, etc."""
+    if input_file is None:
+        if sys.stdin.isatty():
+            output = [
+                context.command.get_usage(context),
+                (
+                    "Error: at least one text file must be passed "
+                    "either through the -i/--input_file option or through a shell pipe."
+                ),
+            ]
+            click.echo("\n\n".join(output))
+            context.exit(-1)
+        else:
+            input_file = click.open_file("-")
+    if output_file is None:
+        output_file = click.open_file("-", mode="w")
+
+    for chunk in api_client.filter(input_file, noise_only=noise_only):
+        output_file.write(ANSI_MARKUP(chunk))
+
+
+@click.command(name="help")
+@click.pass_context
+def help_(context):
+    """Show this message and exit."""
+    click.echo(context.parent.get_help())
+
+
+@click.command()
+@click.argument("ip_address", callback=ip_addresses_parameter, nargs=-1)
+@click.option("-k", "--api-key", help="Key to include in API requests")
+@click.option("-i", "--input", "input_file", type=click.File(), help="Input file")
+@pass_api_client
+@click.pass_context
+@handle_exceptions
+def interesting(context, api_client, api_key, input_file, ip_address):
+    """Report one or more IP addresses as "interesting"."""
+    ip_addresses = get_ip_addresses(context, input_file, ip_address)
+    results = [
+        api_client.interesting(ip_address=ip_address) for ip_address in ip_addresses
+    ]
+    return results
+
+
+@ip_lookup_command
+@click.option("-v", "--verbose", count=True, help="Verbose output")
+def ip(
+    context,
+    api_client,
+    api_key,
+    input_file,
+    output_file,
+    output_format,
+    verbose,
+    ip_address,
+):
+    """Query GreyNoise for all information on a given IP."""
+    ip_addresses = get_ip_addresses(context, input_file, ip_address)
+    results = [api_client.ip(ip_address=ip_address) for ip_address in ip_addresses]
+    return results
+
+
+@not_implemented_command
+def pcap():
+    """Get PCAP for a given IP address."""
+
+
+@gnql_command
+def query(
+    context, api_client, api_key, input_file, output_file, output_format, verbose, query
+):
+    """Run a GNQL (GreyNoise Query Language) query."""
+    queries = get_queries(context, input_file, query)
+    results = [api_client.query(query=query) for query in queries]
+    return results
+
+
+@ip_lookup_command
+def quick(
+    context, api_client, api_key, input_file, output_file, output_format, ip_address
+):
+    """Quickly check whether or not one or many IPs are "noise"."""
+    ip_addresses = get_ip_addresses(context, input_file, ip_address)
+    results = []
+    if ip_addresses:
+        results.extend(api_client.quick(ip_addresses=ip_addresses))
+    return results
+
+
+@click.command()
+@click.option("-k", "--api-key", required=True, help="Key to include in API requests")
+@click.option("-t", "--timeout", type=click.INT, help="API client request timeout")
+@click.option("-s", "--api-server", help="API server")
+def setup(api_key, timeout, api_server):
+    """Configure API key."""
+    config = {"api_key": api_key}
+
+    if timeout is None:
+        config["timeout"] = DEFAULT_CONFIG["timeout"]
+    else:
+        config["timeout"] = timeout
+
+    if api_server is None:
+        config["api_server"] = DEFAULT_CONFIG["api_server"]
+    else:
+        config["api_server"] = api_server
+
+    save_config(config)
+    click.echo("Configuration saved to {!r}".format(CONFIG_FILE))
+
+
+@not_implemented_command
+def signature():
+    """Submit an IDS signature to GreyNoise to be deployed to all GreyNoise nodes."""
+
+
+@gnql_command
+def stats(
+    context, api_client, api_key, input_file, output_file, output_format, verbose, query
+):
+    """Get aggregate stats from a given GNQL query."""
+    queries = get_queries(context, input_file, query)
+    results = [api_client.stats(query=query) for query in queries]
+    return results
+
+
+@click.command()
+def version():
+    """Get version and OS information for your GreyNoise commandline installation."""
+    click.echo(
+        "greynoise {}\n"
+        "  Python {}\n"
+        "  {}\n".format(__version__, platform.python_version(), platform.platform())
+    )

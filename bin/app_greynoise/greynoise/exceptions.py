d7e54a20865408119de9113372a5627240ff9206
Andrew Morris <andrew@greynoise.io>
2020-02-03 14:10:37 -0500

    remove old code

--- bin/app_greynoise/greynoise/exceptions.py
+++
@@ -1,9 +0,0 @@
-"""GreyNoise API client exceptions."""
-
-
-class RequestFailure(Exception):
-    """Exception to capture a failed request."""
-
-
-class RateLimitError(RequestFailure):
-    """API rate limit passed."""
